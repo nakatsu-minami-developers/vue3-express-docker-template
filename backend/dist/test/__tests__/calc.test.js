b380f49f427239fed3fe857ad9e1f3239a267c0f
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-08-15 21:18:47 +0900

    .gitignoreに追加したファイルの削除

--- backend/dist/test/__tests__/calc.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const calc_1 = require("../calc");
-describe("plus test", () => {
-    test("1 + 1", () => {
-        expect((0, calc_1.plus)(1, 1)).toEqual(2);
-    });
-});
--- backend/dist/test/calc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.plus = void 0;
-function plus(a, b) {
-    return a + b;
-}
-exports.plus = plus;
