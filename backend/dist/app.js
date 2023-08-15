aedcce3e7af2a648eb879455d21c09e0c60a309f
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-08-15 11:24:09 +0900

    Jest環境構築

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join('views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -16,7 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join('public')));
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
---
+++ backend/dist/test/calc.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plus = void 0;
+function plus(a, b) {
+    return a + b;
+}
+exports.plus = plus;
